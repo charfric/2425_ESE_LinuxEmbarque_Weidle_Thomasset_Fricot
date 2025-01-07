f6ac3b7a0896e4cc614bebaa1b57bc66df8c7d9d
rems <remipbw@gmail.com>
2025-01-07 11:32:37 +0100

    Auto stash before rebase of "main" onto "origin/main"

--- modules/hello.mod.c
+++ modules/hello.mod.c
@@ -1,12 +1,8 @@
-#include <linux/build-salt.h>
 #include <linux/module.h>
 #include <linux/vermagic.h>
 #include <linux/compiler.h>
 
-BUILD_SALT;
-
 MODULE_INFO(vermagic, VERMAGIC_STRING);
-MODULE_INFO(name, KBUILD_MODNAME);
 
 __visible struct module __this_module
 __attribute__((section(".gnu.linkonce.this_module"))) = {
@@ -16,18 +12,6 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 	.exit = cleanup_module,
 #endif
 	.arch = MODULE_ARCH_INIT,
-};
-
-#ifdef CONFIG_RETPOLINE
-MODULE_INFO(retpoline, "Y");
-#endif
-
-static const struct modversion_info ____versions[]
-__used
-__attribute__((section("__versions"))) = {
-	{ 0xd1b09e08, "module_layout" },
-	{ 0x7c32d0f0, "printk" },
-	{ 0xbdfb6dbb, "__fentry__" },
 };
 
 static const char __module_depends[]
